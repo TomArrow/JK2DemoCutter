7cbacfa4c8ad8241d72f6e52f3b26ec5e841388b
Tom <tomarrowtomarrow@hotmail.com>
2022-11-14 18:41:40 +0100

    Get rid of the ASM stuff. Doesn't compile in 64 bit.

--- DemoCutter/common.cpp
+++ DemoCutter/common.cpp
@@ -139,9 +139,9 @@ void QDECL Com_sprintf(char* dest,int size, const char* fmt, ...) {
 	if (len >= size) {
 		Com_Printf("Com_sprintf: overflow of %i in %i\n", len, size);
 #ifdef	_DEBUG
-		__asm {
-			int 3;
-		}
+		//__asm {
+		//	int 3;
+		//}
 #endif
 	}
 }
