52dc7b908c8b589db9528d11a903e26ab1fd5223
Tom <tomarrowtomarrow@hotmail.com>
2023-05-20 09:13:44 +0200

    Add example script for parallel analysis of demos.

---
+++ analyzeParallel.sh
@@ -0,0 +1,10 @@
+#/bin/bash
+
+# Note: Remove the ./ before DemoHighlightFinder.exe if the .exe file isn't in the same folder and if it is in PATH instead
+
+# This following line is for serial (non parallel) analysis of a folder
+#find "demos" -name '*' -type f -exec ./DemoHighlightFinder.exe {} 10000 ctfreturns \;
+
+# This following line is for parallel analysis of a folder with 5 threads (5 files being analyzed at same time)
+find "demos" -name '*' -type f -print0 | xargs -0 -P 5 -I %  ./DemoHighlightFinder.exe % 10000 ctfreturns \;
+read -n1 -r
