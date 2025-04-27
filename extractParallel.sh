793909a82b4e624ae2a75ece5c44e29013123522
Tom <tomarrowtomarrow@hotmail.com>
2025-04-27 14:15:02 +0200

    clarify to replaace quotation marks in extractparallel

--- extractParallel.sh
+++ extractParallel.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #use this to run the highlightExtractionScript in parallel.
-#Make sure to strip/replace ! exclamation marks (from playernames) from the .bat file and switch to Linux file endings.
+#Make sure to strip/replace ! exclamation marks and '` quotation marks (from playernames/chatmessages) from the .bat file and switch to Linux file endings.
 
 function_name () {
 	eval "./$1"
